a0368d3ddbfd85037d9a13eec8c9cfa2ed1a3107
scott-ritchie-1394 <cheasescott@gmail.com>
2016-10-04 19:57:23 +0000

    Initial Commit

---
+++ serielCom.py
@@ -0,0 +1,48 @@
+import time
+import serial
+import os.path
+try:
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+except:
+    ser = serial.Serial('/dev/ttyACM1', 115200)
+
+time.sleep(.1)
+
+
+
+
+def speed(spd):
+    ser.write('A')
+    if spd == 0:
+        ser.write('000')
+    elif spd<10:
+        ser.write('00'+str(spd))
+    elif spd <100:
+        ser.write('0'+str(spd))   
+    else:
+        ser.write(str(spd))
+        
+
+def steer(ste):
+    ser.write('B')
+    if ste == 0:
+        ser.write('000')
+    elif ste<10:
+        ser.write('00'+str(ste))   
+    elif ste <100:
+        ser.write('0'+str(ste))
+    else:
+        ser.write(str(ste))
+
+def direct(drt):
+    ser.write('C')
+    ser.write(str(drt))
+
+
+
+
+steer(10)
+        
+
+
+
